e0a5c6613c3bf08b2310fcf342765ac0a8be3cc0
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2018-12-12 15:24:34 -0300

    Add buttons for import default data and clear all data that has been saved in cache

--- src/scripts/categories.js
+++ src/scripts/categories.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
   $('.categories').addClass('active');
+  $('.tabs').tabs();
 });
--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   $('.settings').addClass('active');
+  $('.collapsible').collapsible();
+});
+
+$("#clear-data").click(function() {
+  localStorage.clear;
+  alert("All data has been successfully cleaned!");
+});
+$("#import-categories").on('click', function (){
+  $.getJSON('default_categories.json', function(json) {
+    localStorage.setItem("expenses", JSON.stringify(json.expenses));
+    localStorage.setItem("incomes", JSON.stringify(json.incomes));
+    localStorage.setItem("accounts", JSON.stringify(json.accounts));
+    alert("Categories imported successfully!")
+  });
 });
