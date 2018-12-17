b2c758f58c70f5b4829dbfd8688292c7f68d18cd
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2018-12-17 18:22:24 -0300

    Update cache version

--- src/root-files/sw.js
+++ src/root-files/sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheVersion = 4;
+const cacheVersion = 5;
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
