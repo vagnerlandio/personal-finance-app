b46a1e9e0d8d26101915a86e2cce7df613099a73
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2018-12-17 22:52:08 -0300

    Create a button to change the viewed month content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,64 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.overview').addClass('active');
   $('.fixed-action-btn').floatingActionButton({
-    hoverEnabled:false
+    hoverEnabled: false
   });
   $('.modal').modal();
+  updateDate();
+  $("#prev-month").on('click', function(event) {
+    event.preventDefault();
+    prevMonth();
+  });
+  $("#next-month").on('click', function(event) {
+    event.preventDefault();
+    nextMonth();
+  });
 });
+
+// Change month
+var today = new Date();
+var month = today.getMonth() + 1; //January is 0!
+var year = today.getFullYear();
+var currentMonth = month,
+  currentYear = year;
+var months = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+}
+
+function updateDate() {
+  $("#current-month").text(months[currentMonth]);
+  $("#current-year").text(currentYear);
+}
+
+function prevMonth() {
+  if (currentMonth > 1) {
+    currentMonth--
+  } else {
+    currentMonth = 12;
+    currentYear--;
+  }
+  updateDate();
+  $('main').fadeOut(150, () => $('main').fadeToggle(150));
+}
+
+function nextMonth() {
+  if (currentMonth < 12) {
+    currentMonth++
+  } else {
+    currentMonth = 1;
+    currentYear++;
+  }
+  updateDate();
+  $('main').fadeOut(150, () => $('main').fadeToggle(150));
+}
