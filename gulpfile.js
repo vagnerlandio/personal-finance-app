de7b23a246ba43afe44b3e74da743b69b64aa48e
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2018-12-30 23:48:18 -0300

    Convert gulpfile.js to the Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-const gulp    = require('gulp');
-const sass    = require('gulp-sass');
-const pug     = require('gulp-pug');
-const merge   = require('merge-stream');
-const del     = require('del');
-const bs      = require('browser-sync').create();
+const { src, dest, watch, series, parallel }    = require('gulp');
+const sass        = require('gulp-sass');
+const pug         = require('gulp-pug');
+const merge       = require('merge-stream');
+const del         = require('del');
+const browserSync = require('browser-sync');
+const server      = browserSync.create();
 
 // AUX VARIABLES & FUNCTIONS
 const watchFiles = [
@@ -15,17 +16,9 @@ const watchFiles = [
 ];
 
 const watchDistFiles = [
-  '**/*.html',
-  'js/**/*.js',
-  'css/**/*.css',
-];
-
-const tasks = [
-  'images',
-  'scripts',
-  'resources',
-  'styles',
-  'views',
+  'dist/**/*.html',
+  'dist/js/**/*.js',
+  'dist/css/**/*.css',
 ];
 
 function imagesPath(param) {
@@ -55,83 +48,124 @@ function viewsPath(param) {
 
 // TASKS
 // Task 'del' - Run with command 'gulp del'
-gulp.task('del', () => {
+function clean(done) {
   del(['dist']);
-});
+  done();
+}
 
 // Task 'images' - Run with command 'gulp images'
-gulp.task('images', () => {
-  gulp.src(imagesPath('*.*'))
-    .pipe(gulp.dest('dist/img'));
-});
+function images(done) {
+  src(imagesPath('*.*'))
+    .pipe(dest('dist/img'));
+  done();
+}
 
 // Task 'scripts' - Run with command 'gulp scripts'
-gulp.task('scripts', () => {
-  let pages = gulp.src(javascriptsPath('pages/*.js'))
-    .pipe(gulp.dest('dist/js'));
+function scriptsPages(done) {
+  src(javascriptsPath('pages/*.js'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+  done();
+}
 
-  let vendor = gulp.src(javascriptsPath('vendor/*.js'))
-    .pipe(gulp.dest('dist/js/vendor'));
+function scriptsVendor(done) {
+  src(javascriptsPath('vendor/*.js'))
+    .pipe(dest('dist/js/vendor'))
+    .pipe(browserSync.stream());
+  done();
+}
 
-  let sw = gulp.src(javascriptsPath('service-worker.js'))
-    .pipe(gulp.dest('dist'));
+function scriptsSw(done) {
+  src(javascriptsPath('service-worker.js'))
+    .pipe(dest('dist'));
+  done();
+}
 
-  let app = gulp.src(javascriptsPath('application.js'))
-    .pipe(gulp.dest('dist/js'));
+function scriptsApp(done) {
+  src(javascriptsPath('application.js'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+  done();
+}
 
-  merge(pages, vendor, sw, app);
-});
+function scripts(done) {
+  series(scriptsPages, scriptsVendor, scriptsSw, scriptsApp)(done);
+}
 
 // Task 'resources' - Run with command 'gulp resources'
-gulp.task('resources', () => {
-  gulp.src(resourcesPath('*.*'))
-    .pipe(gulp.dest('dist/res'));
-});
+function resources(done) {
+  src(resourcesPath('*.*'))
+    .pipe(dest('dist/res'));
+  done();
+}
 
 // Task 'styles' - Run with command 'gulp styles'
-gulp.task('styles', () => {
-  let pages = gulp.src(stylesheetsPath('pages/*.scss'))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+function styles(done) {
+  function pages(done) {
+    src(stylesheetsPath('pages/*.scss'))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(dest('dist/css'))
+      .pipe(browserSync.stream());
+    done();
+  }
 
-  // let vendor = gulp.src(stylesheetsPath('vendor/*.scss'))
-  //   .pipe(sass().on('error', sass.logError))
-  //   .pipe(gulp.dest('dist/css'));
+  function app(done) {
+    src(stylesheetsPath('application.scss'))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(dest('dist/css'))
+      .pipe(browserSync.stream());
+    done();
+  }
 
-  let app = gulp.src(stylesheetsPath('application.scss'))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
-
-  merge(pages,/* vendor,*/ app, bs.reload({ stream: true }));
-});
+  series(pages, app)(done);
+}
 
 // Task 'views' - Run with command 'gulp views'
-gulp.task('views', () => {
-  gulp.src(viewsPath('pages/*.pug'))
+function views(done) {
+  src(viewsPath('pages/*.pug'))
     .pipe(pug())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'));
+  done();
+}
 
 // SERVER TASK
 // Task 'watch' - Run with command 'gulp watch'
-gulp.task('watch', () => {
-  gulp.watch(watchFiles, tasks);
-});
+function watcher() {
+  watch(watchFiles, series(images, scripts, resources, styles, views));
+}
 
-gulp.task('server', tasks, () => {
+function reload(done) {
+  server.reload();
+  done();
+}
 
-  bs.init({
+function serve(done) {
+  server.init({
     server: {
       baseDir: 'dist',
     },
   });
+  done();
+}
 
-  gulp.watch(watchFiles, tasks);
-  gulp.watch(watchDistFiles).on('change', bs.reload);
-
-  // gulp.watch(watchFiles).on('change', bs.reload);
-
-});
+exports.clean = clean;
+exports.images = images;
+exports.scripts = scripts;
+exports.resources = resources;
+exports.styles = styles;
+exports.views = views;
+exports.watcher = watcher;
+exports.reload = reload;
+exports.serve = serve;
 
 // MAIN TASKS
-gulp.task('default', tasks);
+exports.build = series(
+  clean,
+  parallel(
+    images,
+    scripts,
+    resources,
+    styles,
+    views
+  )
+);
