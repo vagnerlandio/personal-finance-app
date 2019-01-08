fe6f79d197557ea36deae660634b5a39de3c35bf
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-08 15:57:39 -0300

    Add created transfers to the database

--- src/assets/javascripts/components/databases/transfers.js
+++ src/assets/javascripts/components/databases/transfers.js
@@ -1,5 +1,19 @@
 // TRANSFERS
 // Create
+function createTransfer() {
+  dbTransfers.put({
+    description: $('#transfer-description').val(),
+    amount: $('#transfer-amount').val(),
+    due_date: $('#transfer-due-date').val(),
+    status: $('#transfer-status').is(':checked'),
+    origin: $('#transfer-origin').find(':selected').text(),
+    destination: $('#transfer-destination').find(':selected').text(),
+  }).then(function (response) {
+    alert('Transfer "' + response.id + '" created with successus!');
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
 
 // Read
 
