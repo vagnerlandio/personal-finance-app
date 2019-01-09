ccba474d2e5438662809da39ad55de2945ce777a
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-08 23:08:50 -0300

    Add created incomes to the database

--- src/assets/javascripts/components/databases/incomes.js
+++ src/assets/javascripts/components/databases/incomes.js
@@ -1,5 +1,19 @@
 // INCOMES
 // Create
+function createIncome() {
+  dbIncomes.put({
+    description: $('#income-description').val(),
+    amount: $('#income-amount').val(),
+    due_date: $('#income-due-date').val(),
+    status: $('#income-status').is(':checked'),
+    category: $('#income-category').find(':selected').text(),
+    account: $('#income-account').find(':selected').text(),
+  }).then(function (response) {
+    alert('Income "' + response.id + '" created with successus!');
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
 
 // Read
 
