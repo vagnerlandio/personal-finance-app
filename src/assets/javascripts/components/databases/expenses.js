83962cdd8bd5942f552f4dd3caa100819fcd28fa
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-08 23:26:15 -0300

    Add created expenses to the database

--- src/assets/javascripts/components/databases/expenses.js
+++ src/assets/javascripts/components/databases/expenses.js
@@ -1,5 +1,19 @@
 // EXPENSES
 // Create
+function createExpense() {
+  dbExpenses.put({
+    description: $('#expense-description').val(),
+    amount: $('#expense-amount').val(),
+    due_date: $('#expense-due-date').val(),
+    status: $('#expense-status').is(':checked'),
+    category: $('#expense-category').find(':selected').text(),
+    account: $('#expense-account').find(':selected').text(),
+  }).then(function (response) {
+    alert('Expense "' + response.id + '" created with successus!');
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
 
 // Read
 
