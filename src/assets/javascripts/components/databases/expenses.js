09b949f5e6f9454d045b3168e75ddb85fffcd4b9
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-09 17:49:38 -0300

    Load expense categories from localStorage

--- src/assets/javascripts/components/databases/expenses.js
+++ src/assets/javascripts/components/databases/expenses.js
@@ -1,9 +1,23 @@
 // EXPENSES
+$(() => {
+  loadExpenseCategories();
+});
+
 function cleanExpenseForm() {
   $('#expense-description').val('');
   $('#expense-amount').val('');
   $('#expense-due-date').val('');
   $('#expense-status').attr('checked', false);
+}
+
+function loadExpenseCategories() {
+  let categories = JSON.parse(localStorage.getItem('expenses'));
+
+  $.each(categories, (index, value) => {
+    $('#expense-category').append(new Option(value, value.toLowerCase()));
+  });
+
+  $('select').formSelect();
 }
 
 // Create
