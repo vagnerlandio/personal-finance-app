17e6c5d9d62bc4cc0318506dde6f459906b18356
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-14 18:49:19 -0300

    Create default accounts in the first startup

---
+++ src/assets/javascripts/components/startup_configs/create_default_accounts.js
@@ -0,0 +1,4 @@
+function createDefaultAccounts() {
+  createAccount('My Wallet', 0.00, 'Wallet');
+  console.log('Default Accounts created with successus!');
+}
--- src/assets/javascripts/components/startup_configs/main.js
+++ src/assets/javascripts/components/startup_configs/main.js
@@ -3,6 +3,7 @@ window.onload = () => {
     console.log('Content loaded from the database!');
   } else {
     createDefaultCategories();
+    createDefaultAccounts();
 
     localStorage.setItem('hasCodeRunBefore', true);
     console.log('Content saved to the database!');
