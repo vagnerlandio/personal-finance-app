45053919d390dc09b076c2e5627876868eab0493
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-01-18 00:43:34 -0300

    List expense for the current month in the transactions page

--- src/assets/javascripts/pages/transactions.js
+++ src/assets/javascripts/pages/transactions.js
@@ -4,4 +4,50 @@ $(() => {
     hoverEnabled:false
   });
   updateDate();
+  loadExpenses();
+  $('.tabs').tabs({swipeable: true});
+  $('#prev-month').on('click', function(event) {
+    event.preventDefault();
+    $('.collection-item').remove();
+    loadExpenses();
+  });
+  $('#next-month').on('click', function(event) {
+    event.preventDefault();
+    $('.collection-item').remove();
+    loadExpenses();
+  });
 });
+
+function loadExpenses() {
+  dbExpenses.allDocs({
+    include_docs: true,
+  }).then(function (result) {
+    $.each(result.rows, function(index, el) {
+      if (el.doc.description !== undefined) {
+        if ((monthsAbbr[currentMonth] === el.doc.due_date.slice(0, 3)) & (el.doc.due_date.slice(-4) == currentYear)) {
+          createTransactionCollectionItem(el.doc.description, el.doc.amount, el.doc.status);
+        }
+      }
+    });
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+function createTransactionCollectionItem(description, amount, status) {
+  let statusIcon;
+
+  if (status) {
+    statusIcon = 'close';
+  } else {
+    statusIcon = 'check';
+  }
+
+  let elem = `<li class="collection-item avatar">
+    <i class="material-icons circle">${statusIcon}</i>
+    <span class="title">${description}</span>
+    <p>Amount: <span class="right">${amount}</span></p>
+  </li>`;
+
+  $('#expenses-page > ul').append(elem);
+}
